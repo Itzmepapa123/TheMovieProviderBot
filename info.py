8b253a7c59fdb819bd233465e57f28a61351dcc9
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-01 21:18:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,13 +55,13 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "nami")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Thepower88links/8') # How to open tutorial link for verification
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://publicearn.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7d3be3c42438436ade8b7a491d42a4ffcad6be5b')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
