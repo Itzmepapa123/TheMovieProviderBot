f99e0164b82f60247aa667fd3f83ca0424c95e2e
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-01 21:54:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Thepower88links/8') # How
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://publicearn.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7d3be3c42438436ade8b7a491d42a4ffcad6be5b')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
